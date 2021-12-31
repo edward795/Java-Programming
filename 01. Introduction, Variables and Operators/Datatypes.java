b2c708cb76fbf35f57a816721ca3760eac8b33a5
edward795 <ayyappadasgb777999@gmail.com>
2021-12-31 17:50:58 +0530

    1st commit

---
+++ 01. Introduction, Variables and Operators/Datatypes.java
@@ -0,0 +1,10 @@
+class Datatypes{
+    public static void main(String[] args){;
+        byte marks=90;
+        float pi=3.14f;
+        float div=15.0f/4.0f;
+        long view=10000000;
+        char gender='M';
+        System.out.println(view);
+    }
+}
