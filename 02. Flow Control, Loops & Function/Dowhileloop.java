7d3df5b7b9c0faa7be57cdbb23e4400b83b16e22
edward795 <ayyappadasgb777999@gmail.com>
2022-01-22 10:22:50 +0530

    10th commit

---
+++ 02. Flow Control, Loops & Function/Dowhileloop.java
@@ -0,0 +1,9 @@
+public class Dowhileloop{
+    public static void main(String[] args){
+        int i=0;
+        do{
+            System.out.println("Hello");
+            i++;
+        }while(i<10);
+    }
+}
