8898ed3915333f83628af828a181433b1acde727
edward795 <ayyappadasgb777999@gmail.com>
2022-01-26 08:26:38 +0530

    14th commit

---
+++ 03. Arrays & Strings/Array_Declaration.java
@@ -0,0 +1,35 @@
+class Array_Declaration{
+    public static void main(String[] args){
+        int[] a={1,2,3,4,5};
+        System.out.println(a[2]);
+        System.out.println(a[1]);
+
+        //Ways of declaring arrays
+        int[] b;
+        b= new int[3];
+        a[0]=1;
+        a[1]=2;
+        a[2]=3;
+
+        int[] c = new int[3];
+        c[0]=1;
+        c[1]=2;
+        c[2]=3;
+
+
+        int[] d = {1,2,3};
+
+        //[]a vs a[] in java
+
+        int[] e = new int[4];
+        int[] f = new int[4]; 
+
+        int[] g;
+        g=new int[4];
+
+        int l[],k;
+        l=new int[4];
+        k=10;
+
+    }
+}
