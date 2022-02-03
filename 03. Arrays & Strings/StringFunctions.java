623a88534bce27bdb3bf2d593d95234260ac4358
edward795 <ayyappadasgb777999@gmail.com>
2022-02-03 07:57:39 +0530

    17th commit

---
+++ 03. Arrays & Strings/StringFunctions.java
@@ -0,0 +1,21 @@
+public class StringFunctions {
+    public static void main(String[] args){
+       String s1="geeksforgeeks";
+       String s2="for";
+
+       //System.out.println(s1.contains(s2));
+       //System.out.println(s1.equals(s2));
+    //    int res=s1.compareTo(s2);
+    //    if(res==0) 
+    //        System.out.println("Same");
+    //    if(res>0)
+    //        System.out.println("s1 Greater");
+    //    if(res<0)
+    //        System.out.println("s1 Smaller");
+
+    System.out.println(s1.indexOf(s2));
+
+    
+
+    }
+}
